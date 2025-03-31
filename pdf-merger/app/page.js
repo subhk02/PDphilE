8b9f144d15f7b6a8acbb196421502444a23ba555
subham <subhampatro9122@gmail.com>
2025-03-31 12:04:02 +0530

    Updated Page

--- pdf-merger/app/page.js
+++ pdf-merger/app/page.js
@@ -77,6 +77,8 @@ export default function Home() {
     setFiles([]);
   };
 
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
   const mergePDFs = async () => {
     if (files.length === 0) {
       setError('Please add PDF files to merge');
@@ -94,7 +96,7 @@ export default function Home() {
     });
 
     try {
-      const response = await fetch('http://localhost:5000/api/merge', {
+      const response = await fetch(`${API_URL}/api/merge`, {
         method: 'POST',
         body: formData,
       });
